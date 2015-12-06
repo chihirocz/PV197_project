3fa4b05067419ce89a74552513c811b8bac4c69f
chihirocz <pursuer@seznam.cz>
2015-12-06 22:38:19 +0100

    better approach, not working

--- kernel.cu
+++ kernel.cu
@@ -1,8 +1,8 @@
 // write your code into this file
 
-#define TILE_X 2
-#define TILE_Y 2
-#define TILE_Z 32
+#define TILE_X 3
+#define TILE_Y 3
+#define TILE_Z 16
 #define PADDING 0
 
 __global__ void compute_cell(int* in_array, int* out_array, int dim);
@@ -10,7 +10,7 @@ __global__ void compute_cell(int* in_array, int* out_array, int dim);
 void solveGPU(int **dCells, int dim, int iters)
 {
 	dim3 threadsPerBlock(TILE_X, TILE_Y, TILE_Z);
-	dim3 numBlocks(dim/threadsPerBlock.x, dim/threadsPerBlock.y, dim/threadsPerBlock.z);
+	dim3 numBlocks((int)ceil(dim/(float)(TILE_X-2)), (int)ceil(dim/(float)(TILE_Y-2)), (int)ceil(dim/(float)(TILE_Z-2)));
 	
 	int* result_array;
 	cudaMalloc((void**)&result_array, dim*dim*dim*sizeof(int));
@@ -41,93 +41,34 @@ void solveGPU(int **dCells, int dim, int iters)
 __global__ void compute_cell(int* in_array, int* out_array, int dim)
 {
 	// using subsegment of 3x3x32 (+2 bordering) cells from entire cube to use coalesced global mem access, i.e. 9 segments
-	__shared__ int tile[TILE_X+2][TILE_Y+2][TILE_Z+2+PADDING];
+	__shared__ int tile[TILE_X][TILE_Y][TILE_Z];
 	
 	//int tx = threadIdx.x;
 
-    int idx_x = blockIdx.x*blockDim.x + threadIdx.x;
-    int idx_y = blockIdx.y*blockDim.y + threadIdx.y;
-    int idx_z = blockIdx.z*blockDim.z + threadIdx.z;
+    int idx_x = blockIdx.x*(blockDim.x-2) + threadIdx.x;
+    int idx_y = blockIdx.y*(blockDim.y-2) + threadIdx.y;
+    int idx_z = blockIdx.z*(blockDim.z-2) + threadIdx.z;
+	
+    int border_idx_x = blockIdx.x*blockDim.x + threadIdx.x;
+    int border_idx_y = blockIdx.y*blockDim.y + threadIdx.y;
+    int border_idx_z = blockIdx.z*blockDim.z + threadIdx.z;
 	
 	int dim2 = dim*dim;
 
 	// loading non-boundary cells into shared
-	tile[threadIdx.x+1][threadIdx.y+1][threadIdx.z+1] = in_array[(idx_x*dim2)+(idx_y*dim)+idx_z];
+//	int is_border = 0;
+//	is_border += 0;
 
-	
-	// loading non-diagonal boundary cells into shared
-	
-	// X-coords
-	if (threadIdx.x == 0)
+	// loading tile to shared
+	tile[threadIdx.x][threadIdx.y][threadIdx.z] = 0;
+	if (idx_x < dim && idx_y < dim && idx_z < dim)
 	{
-		if (idx_x == 0)
+		if (border_idx_x > 0 && border_idx_y > 0 && border_idx_z > 0)
 		{
-			tile[threadIdx.x][threadIdx.y+1][threadIdx.z+1] = 0;
-		}
-		else
-		{
-			tile[threadIdx.x][threadIdx.y+1][threadIdx.z+1] = in_array[((idx_x-1)*dim2)+(idx_y*dim)+idx_z];
+			tile[threadIdx.x][threadIdx.y][threadIdx.z] = in_array[(idx_x*dim2)+(idx_y*dim)+idx_z];
 		}
 	}
-	if (threadIdx.x == (TILE_X-1))
-	{
-		if (idx_x == (dim-1))
-		{
-			tile[threadIdx.x+2][threadIdx.y+1][threadIdx.z+1] = 0;
-		}
-		else
-		{
-			tile[threadIdx.x+2][threadIdx.y+1][threadIdx.z+1] = in_array[((idx_x+1)*dim2)+(idx_y*dim)+idx_z];
-		}
-	}
-	
-	// Y-coords
-	if (threadIdx.y == 0)
-	{
-		if (idx_y == 0)
-		{
-			tile[threadIdx.x+1][threadIdx.y][threadIdx.z+1] = 0;
-		}
-		else
-		{
-			tile[threadIdx.x+1][threadIdx.y][threadIdx.z+1] = in_array[(idx_x*dim2)+((idx_y-1)*dim)+idx_z];
-		}
-	}
-	if (threadIdx.x == (TILE_Y-1))
-	{
-		if (idx_y == (dim-1))
-		{
-			tile[threadIdx.x+1][threadIdx.y+2][threadIdx.z+1] = 0;
-		}
-		else
-		{
-			tile[threadIdx.x+1][threadIdx.y+2][threadIdx.z+1] = in_array[(idx_x*dim2)+((idx_y+1)*dim)+idx_z];
-		}
-	}
-	
-	// Z-coords
-	if (threadIdx.z == 0)
-	{
-		if (idx_z == 0)
-		{
-			tile[threadIdx.x+1][threadIdx.y+1][threadIdx.z] = 0;
-		}
-		else
-		{
-			tile[threadIdx.x+1][threadIdx.y+2][threadIdx.z] = in_array[(idx_x*dim2)+(idx_y*dim)+idx_z-1];
-		}
-	}
-	if (threadIdx.x == (TILE_Z-1))
-	{
-		if (idx_z == (dim-1))
-		{
-			tile[threadIdx.x+1][threadIdx.y+1][threadIdx.z+2] = 0;
-		}
-		else
-		{
-			tile[threadIdx.x+1][threadIdx.y+1][threadIdx.z+2] = in_array[(idx_x*dim2)+(idx_y*dim)+idx_z+1];
-		}
-	}
+
 	
 	__syncthreads();
 	
